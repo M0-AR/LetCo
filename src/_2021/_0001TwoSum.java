4db91d3c5cbf4d4d9c7b59677d0a8901669eab16
m0-ar <s176492@student.dtu.dk>
2021-07-10 22:32:57 +0200

    1. TwoSum problem

---
+++ src/_2021/_0001TwoSum.java
@@ -0,0 +1,25 @@
+package _2021;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class _0001TwoSum {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9)));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i]) && i != map.get(target - nums[i]))
+                return new int[] {i, map.get(target - nums[i])};
+        }
+        return new int[2];
+    }
+}
