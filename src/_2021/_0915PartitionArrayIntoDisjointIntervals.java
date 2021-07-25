e958017a2feec68e3c64fbc2b591237f7aca9e17
m0-ar <s176492@student.dtu.dk>
2021-07-25 08:05:54 +0200

    complete solution.

--- src/_2021/_0915PartitionArrayIntoDisjointIntervals.java
+++ src/_2021/_0915PartitionArrayIntoDisjointIntervals.java
@@ -2,20 +2,27 @@ package _2021;
 
 public class _0915PartitionArrayIntoDisjointIntervals {
     public static void main(String[] args) {
-
+        System.out.println(partitionDisjoint(new int[]{5,0,3,8,6}));
+        System.out.println(partitionDisjoint(new int[]{1,1,1,0,6,12}));
+        System.out.println(partitionDisjoint(new int[]{5,0,3,8,6}));
     }
 
     public static int partitionDisjoint(int[] nums) {
-        int maxIndex = 0;
-        int max = nums[0];
+        int leftMax = 0;
 
-        for (int i = 1; i < nums.length / 2; i++) {
-            if (max < nums[i]) {
-                max = nums[i];
-                maxIndex = i;
-            }
+        for (int i = 0; i < nums.length; i++) {
+
+           leftMax = Math.max(leftMax, nums[i]);
+           int rightMin = Integer.MAX_VALUE;
+
+           for (int j = i + 1; j < nums.length; j++) {
+                rightMin = Math.min(rightMin, nums[j]);
+           }
+
+           if (leftMax <= rightMin)
+               return i + 1;
         }
 
-        return -1;
+        return nums.length;
     }
 }
