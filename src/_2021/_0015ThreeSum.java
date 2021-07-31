5c0d4cf0737868d19270e40cac33468cb552013e
m0-ar <s176492@student.dtu.dk>
2021-07-31 12:17:20 +0200

    Right solution, but need to be optimize.

---
+++ src/_2021/_0015ThreeSum.java
@@ -0,0 +1,36 @@
+package _2021;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class _0015ThreeSum {
+    public static void main(String[] args) {
+        System.out.println(threeSum(new int[]{-1,0,1,2,-1,-4}));
+        System.out.println(threeSum(new int[]{}));
+        System.out.println(threeSum(new int[]{0}));
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        int listIndex = 0;
+
+        if (nums.length == 0) return list;
+        else if (nums.length == 1 && nums[0] == 0) return list;
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    int zero = nums[i] + nums[j] + nums[k];
+                    if (zero == 0)
+                        if (!list.contains(Arrays.asList(nums[i], nums[j], nums[k])))
+                            list.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                }
+            }
+        }
+
+        return list;
+    }
+}
