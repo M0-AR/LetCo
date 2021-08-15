ec2655fc67fd0f47dea0f81a35ea51d449a1c496
m0-ar <s176492@student.dtu.dk>
2021-08-15 21:33:59 +0200

    Done.

--- src/_2021/_0021MergeTwoSortedLists.java
+++ src/_2021/_0021MergeTwoSortedLists.java
@@ -10,56 +10,33 @@ public class _0021MergeTwoSortedLists {
     }
 
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode merge = new ListNode();
+        ListNode merge = new ListNode(0);
+        ListNode currentNode = merge;
         ListNode p1 = l1;
         ListNode p2 = l2;
-        ListNode head = merge;
 
         while (p1 != null && p2 != null) {
+
             if (p1.val < p2.val) {
-                merge.val = p1.val;
+                currentNode.next = p1;
                 p1 = p1.next;
-                if (p2 != null) {
-                    merge.next = new ListNode();
-                    merge = merge.next;
-                }
-            } else if (p2.val < p1.val) {
-                merge.val = p2.val;
-                p2 = p2.next;
-                if (p2 != null) {
-                    merge.next = new ListNode();
-                    merge = merge.next;
-                }
             } else {
-                merge.val = p1.val;
-                p1 = p1.next;
-                if (p2 != null) {
-                    merge.next = new ListNode();
-                    merge = merge.next;
-                }
-            }
-        }
-
-        while (p1 != null) {
-            merge.val = p1.val;
-            p1 = p1.next;
-            if (p2 != null) {
-                merge.next = new ListNode();
-                merge = merge.next;
-            }
-        }
-
-        while (p2 != null) {
-            merge.val = p2.val;
-            p2 = p2.next;
-            if (p2 != null) {
-                merge.next = new ListNode();
-                merge = merge.next;
+                 currentNode.next = p2;
+                p2 = p2.next;
             }
 
+            currentNode = currentNode.next;
         }
 
-        return head;
+        if (p1 != null) {
+            currentNode.next = p1;
+        }
+
+        if (p2 != null) {
+            currentNode.next = p2;
+        }
+
+        return merge.next;
     }
 
    static class ListNode {
